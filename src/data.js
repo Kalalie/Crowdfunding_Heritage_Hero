dc9c0d11ba6771c7f49254c682dc519a1cfcd184
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-09-19 11:20:30 +0800

    All projects/ one Projects pages

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const allProjects = [
+  {
+    id: 1,
+    title: "Project tw11111111o",
+    description: "The second project.",
+    goal: 150,
+    image: "https://via.placeholder.com/300.jpg",
+    is_open: true,
+    date_created: "2020-08-21T14:28:23.382748Z",
+    owner: 1,
+  },
+];
+
+export const oneProject = {
+  id: 1,
+  title: "Project tw11111111o",
+  description: "The second project.",
+  goal: 150,
+  image: "https://via.placeholder.com/300.jpg",
+  is_open: true,
+  date_created: "2020-08-21T14:28:23.382748Z",
+  owner: 1,
+};
