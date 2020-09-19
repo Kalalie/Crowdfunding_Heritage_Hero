22ac6fcda5e00cdf786b6d5395ff051031d763dc
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-09-19 10:40:58 +0800

    Nav Bar (basics)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Nav from "./Components/Nav/Nav";
+import Home from "./Pages/HomePage";
+import Projects from "./Pages/Projects";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Nav />
+
+        <Switch>
+          <Route path="/Projects">
+            <Projects />
+          </Route>
+          <Router path="/">
+            <Home />
+          </Router>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
