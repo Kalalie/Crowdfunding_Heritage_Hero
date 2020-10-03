21cd4d6c748a85d794893c0b66be27342ae1eae1
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-10-03 10:52:11 +0800

    Pledges working and fixed issue with pledges in backend

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Moment from "react-moment";
+import Pledges from "../Components/Pledges/Pledges";
 
 function Projects() {
   const [projectData, setProjectData] = useState({ pledges: [] });
@@ -18,19 +20,24 @@ function Projects() {
   return (
     <div>
       <h2>{projectData.title}</h2>
-      <h3>Created at: {projectData.date_created}</h3>
-      <h3>Status: ${projectData.is_open}</h3>
+      {console.log("hi", projectData)}
+      <h3>
+        Created at:
+        <Moment format="DD/MM/YYYY">{projectData.date_created}</Moment>
+      </h3>
+      <h3>{`Status: ${projectData.is_open ? "Open" : "Closed"}`}</h3>
       <h3>Pledges:</h3>
       <ul>
         {projectData.pledges &&
           projectData.pledges.map((pledgeData, key) => {
             return (
               <li>
-                {pledgeData.amount} from {pledgeData.supporter}
+                {pledgeData.amount} from {pledgeData.supporter_id}
               </li>
             );
           })}
       </ul>
+      <Pledges />
     </div>
   );
 }
